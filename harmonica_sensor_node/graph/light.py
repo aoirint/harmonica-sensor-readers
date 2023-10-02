6c1f511a7a984d842408a186a31a93767560a5a9
aoirint <aoirint@gmail.com>
2023-10-02 14:40:15 +0900

    Add linter and fix format (#32)
    * poetry add --group dev pysen black isort flake8 flake8-bugbear mypy pytest
    
    * configure pysen
    
    * pysen run format
    
    * doc: code format
    
    * remove unused import
    
    * poetry add --group dev types-requests
    
    * poetry add --group dev types-pytz
    
    * poetry add --group dev types-python-dateutil
    
    * fix E402 module level import not at top of file
    
    * sql heredoc
    
    * fix B950 line too long
    
    * fix B001 Do not use bare `except:`
    
    * remove unused pass
    
    * main script: fix no-untyped-def
    
    * graph/temperature: fix no-untyped-def
    
    * greaph/mhz19_temperature: fix no-untyped-def
    
    * graph/mhz19_co2: fix no-untyped-def
    
    * graph/light: fix no-untyped-def
    
    * graph/humidity: fix no-untyped-def
    
    * ignore untyped-call matplotlib.dates.DateFormatter

--- harmonica_sensor_node/graph/light.py
+++ harmonica_sensor_node/graph/light.py
@@ -1,69 +1,77 @@
-import os
+import logging
 import sqlite3
-from datetime import datetime as dt
-from datetime import timedelta
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import Any
+
 import dateutil.parser as dtparser
+import matplotlib
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from pytz import timezone
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.ticker as ticker
-
-from pathlib import Path
-import logging
+matplotlib.use("Agg")
 logger = logging.getLogger(__name__)
 
 
-def _getData(cur, start, end):
+def _getData(cur: sqlite3.Cursor, start: datetime, end: datetime) -> list[Any]:
     rows = []
-    for row in cur.execute('SELECT * FROM sensor'):
+    for row in cur.execute("SELECT * FROM sensor"):
         tsString = row[6]
         ts = dtparser.parse(tsString)
         if start <= ts and ts < end:
-            rows.append(row + (ts, ))
+            rows.append(row + (ts,))
     return rows
 
-def _draw(cur, date, fp):
-    start = date ; end = date + timedelta(days=1)
-    x = [] ; y = []
+
+def _draw(cur: sqlite3.Cursor, date: datetime, fp: Path) -> None:
+    start = date
+    end = date + timedelta(days=1)
+    x = []
+    y = []
     for row in _getData(cur, start, end):
-        ts = row[7] ; val = row[1]
+        ts = row[7]
+        val = row[1]
         if val is None:
             continue
-        
+
         ts_n = ts.replace(tzinfo=None)
 
-        x.append(ts_n) ; y.append(val)
+        x.append(ts_n)
+        y.append(val)
 
     start_n = start.replace(tzinfo=None)
     end_n = end.replace(tzinfo=None)
     date_string = start_n.date().isoformat()
-
 
     plt.clf()
     fig, ax = plt.subplots()
     ax.set_xlim(start_n, end_n)
     ax.set_ylim(0, 1024)
     ax.plot(x, y)
-    #ax.plot([ start, end ], [ 10 * 10**9, ] * 2)
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
+    # ax.plot([ start, end ], [ 10 * 10**9, ] * 2)
+    date_formatter = mdates.DateFormatter(fmt="%H:%M")  # type: ignore[no-untyped-call]
+    ax.xaxis.set_major_formatter(date_formatter)
     ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: x))
-    #ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: '%.1f GB' % (x / (10**9), )))
-    fig.suptitle(f'Light {date_string}')
+    # ax.yaxis.set_major_formatter(
+    #     ticker.FuncFormatter(
+    #         lambda x, pos: "%.1f GB" % (x / (10**9),),
+    #     ),
+    # )
+    fig.suptitle(f"Light {date_string}")
     fig.savefig(fp)
 
     plt.close()
 
 
 def draw_days(
-    cur,
+    cur: sqlite3.Cursor,
     output_dir: Path,
     tz: str,
     days: int = 1,
-):
-    now = dt.now(timezone(tz))
+) -> None:
+    now = datetime.now(timezone(tz))
     today = now.replace(hour=0, minute=0, second=0, microsecond=0)
 
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -71,7 +79,7 @@ def draw_days(
     for dd in range(days):
         date = today - timedelta(days=dd)
         date_string = date.replace(tzinfo=None).date().isoformat()
-        logger.info(f'Light {date_string}')
+        logger.info(f"Light {date_string}")
 
-        path = output_dir / f'{date_string}.png'
+        path = output_dir / f"{date_string}.png"
         _draw(cur, date, path)
--- harmonica_sensor_node/graph/mhz19_temperature.py
+++ harmonica_sensor_node/graph/mhz19_temperature.py
@@ -1,35 +1,38 @@
-import os
+import logging
 import sqlite3
-from datetime import datetime as dt
-from datetime import timedelta
+from datetime import datetime, timedelta
+from pathlib import Path
+from typing import Any
+
 import dateutil.parser as dtparser
+import matplotlib
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 from pytz import timezone
 
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.ticker as ticker
-
-from pathlib import Path
-import logging
+matplotlib.use("Agg")
 logger = logging.getLogger(__name__)
 
 
-def _getData(cur, start, end):
+def _getData(cur: sqlite3.Cursor, start: datetime, end: datetime) -> list[Any]:
     rows = []
-    for row in cur.execute('SELECT * FROM sensor'):
+    for row in cur.execute("SELECT * FROM sensor"):
         tsString = row[6]
         ts = dtparser.parse(tsString)
         if start <= ts and ts < end:
-            rows.append(row + (ts, ))
+            rows.append(row + (ts,))
     return rows
 
-def _draw(cur, date, fp):
-    start = date ; end = date + timedelta(days=1)
-    x = [] ; y = []
+
+def _draw(cur: sqlite3.Cursor, date: datetime, fp: Path) -> None:
+    start = date
+    end = date + timedelta(days=1)
+    x = []
+    y = []
     for row in _getData(cur, start, end):
-        ts = row[7] ; val = row[5]
+        ts = row[7]
+        val = row[5]
         if val is None:
             continue
 
@@ -41,10 +44,11 @@ def _draw(cur, date, fp):
         #
         # float v0 = (float)temperatureRaw / 1023 * VOLT;
         # float temperature = (v0 - V_OFFSET) / DEGREE_PER_VOLT;
-        #val = ((raw/1023*5.0)-0.6)/0.01
-        #val -= 1.5 # hand-fix
+        # val = ((raw/1023*5.0)-0.6)/0.01
+        # val -= 1.5 # hand-fix
 
-        x.append(ts_n) ; y.append(val)
+        x.append(ts_n)
+        y.append(val)
 
     start_n = start.replace(tzinfo=None)
     end_n = end.replace(tzinfo=None)
@@ -55,22 +59,23 @@ def _draw(cur, date, fp):
     ax.set_xlim(start_n, end_n)
     ax.set_ylim(0, 40)
     ax.plot(x, y)
-#    ax.plot([ start, end ], [ 10 * 10**9, ] * 2)
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))
-    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: '%.2f C' % (y, )))
-    fig.suptitle(f'MHZ19 Temperature {date_string}')
+    #    ax.plot([ start, end ], [ 10 * 10**9, ] * 2)
+    date_formatter = mdates.DateFormatter(fmt="%H:%M")  # type: ignore[no-untyped-call]
+    ax.xaxis.set_major_formatter(date_formatter)
+    ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y, pos: "%.2f C" % (y,)))
+    fig.suptitle(f"MHZ19 Temperature {date_string}")
     fig.savefig(fp)
 
     plt.close()
 
 
 def draw_days(
-    cur,
+    cur: sqlite3.Cursor,
     output_dir: Path,
     tz: str,
     days: int = 1,
-):
-    now = dt.now(timezone(tz))
+) -> None:
+    now = datetime.now(timezone(tz))
     today = now.replace(hour=0, minute=0, second=0, microsecond=0)
 
     output_dir.mkdir(parents=True, exist_ok=True)
@@ -78,7 +83,7 @@ def draw_days(
     for dd in range(days):
         date = today - timedelta(days=dd)
         date_string = date.replace(tzinfo=None).date().isoformat()
-        logger.info(f'MHZ19 Temperature {date_string}')
+        logger.info(f"MHZ19 Temperature {date_string}")
 
-        path = output_dir / f'{date_string}.png'
+        path = output_dir / f"{date_string}.png"
         _draw(cur, date, path)
